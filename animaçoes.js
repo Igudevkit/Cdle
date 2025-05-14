d57cfa6c3d5ce3a32208e8632b87bdb58305c9d5
igor <115957786+Igudevkit@users.noreply.github.com>
2025-05-13 21:23:53 -0300

    Update animaçoes.js

--- animaçoes.js
+++ animaçoes.js
@@ -1,1 +1,3 @@
-
+var _0xabc=["value","trim","toLowerCase","eusoudev","nome"];
+devInput.addEventListener("keydown",function(_0xdef){if(_0xdef.key==="Enter"){if(devInput[_0xabc[0]][_0xabc[1]]()[_0xabc[2]]()===_0xabc[3]){alert("Produto secreto: "+target[_0xabc[4]])}else{alert("Código incorreto!")}devInput.value="";
+devContent.classList.remove("show")}});
